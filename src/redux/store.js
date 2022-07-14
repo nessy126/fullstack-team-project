37edf39450c4930d76c7af2ea42dbaa632be439b
ADRIAN TMOCALIUC <adriantmocaliuc@gmail.com>
2022-07-15 00:31:09 +0300

    adding indexes.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./auth";
-import book from "./book";
-// import authSlice from "./auth/authSlice";
-// import bookSlice from "./book/bookSlice";
+import bookSlice from "./book";
 
 const store = configureStore({
   reducer: {
     auth: authSlice,
-    book: book,
+    book: bookSlice,
   },
 });
 
