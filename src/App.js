a1763b53e6c6c5e3cc948fc684a88ad1f064aa38
ADRIAN TMOCALIUC <adriantmocaliuc@gmail.com>
2022-07-15 00:32:58 +0300

    adding indexes.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Container from "components/Container";
-import TrainingPage from "pages/TrainingPage";
+import Container from "./components/Container";
+import TrainingPage from "./pages/TrainingPage";
 
-import MainNav from "components/MainNav";
-import LoginForm from "components/LoginForm";
-import RegisterForm from "components/RegisterForm";
+import MainNav from "./components/MainNav";
+import LoginForm from "./components/LoginForm";
+import RegisterForm from "./components/RegisterForm";
 // import Statistics from "./components/Statistics";
 // import MyGoals from "./components/MyGoals";
 
