148d08cd981ddb56bb4478d250bfaf1f6a5eb60e
Anastasiia <65896682+nessy126@users.noreply.github.com>
2022-07-18 20:21:18 +0200

    Update NotFoundPage.jsx
    correct

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -2,7 +2,7 @@ import s from "./NotFoundPage.module.scss";
 const NotFoundPage = () => {
   return (
     <div className={s.notfound}>
-      <h1>Nothing not found</h1>;
+      <h1>Nothing found</h1>;
     </div>
   );
 };
