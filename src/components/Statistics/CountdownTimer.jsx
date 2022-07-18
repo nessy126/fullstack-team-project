ff34bb5fb7d2baa06e3fe7fb1db24e138191eab3
ADRIAN TMOCALIUC <adriantmocaliuc@gmail.com>
2022-07-18 17:18:31 +0300

    conflicts

--- src/components/Statistics/CountdownTimer.jsx
+++ src/components/Statistics/CountdownTimer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import DateTimeDisplay from "./DateTimeDisplay";
-import { useCountdown } from "../../hooks";
+import { useCountdown } from "hooks/useCountDown";
 import s from "./CountdownTimer.module.scss";
 
 const ExpiredNotice = () => {
