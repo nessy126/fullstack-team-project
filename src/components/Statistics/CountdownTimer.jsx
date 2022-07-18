670f4ceedb79d752ccca17118a50fb913d525641
OlenaPatlata <elenapat468@gmail.com>
2022-07-18 15:17:00 +0300

    changed path

--- src/components/Statistics/CountdownTimer.jsx
+++ src/components/Statistics/CountdownTimer.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import DateTimeDisplay from "./DateTimeDisplay";
 import { useCountdown } from "hooks";
 import s from "./CountdownTimer.module.scss";
-
+  console.log(useCountdown);
 const ExpiredNotice = () => {
+
   return (
     <div className="expired-notice">
       <span>Expired!!!</span>
