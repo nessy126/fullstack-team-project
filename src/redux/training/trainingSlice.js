540b6fcd664174b9dd6f9f81bdb7ee0ebc16f906
nessy126 <nessy126@gmail.com>
2022-07-20 10:27:06 +0200

    del unnes-ry

--- src/redux/training/trainingSlice.js
+++ src/redux/training/trainingSlice.js
@@ -1,37 +1,15 @@
-// import { createSlice } from '@reduxjs/toolkit';
-// import {addTraining, getProgress} from './trainingOperations';
-
-// const initialState = {
-//   training: {
-  
-//   },
-//   finishedTrainings: [],
-//   isLoading: false,
-//   isLoggedIn: false,
-//   error: null,
-// }
-
-const trainingSlice = createSlice({
-  name: 'training',
-  initialState,
-  extraReducers:{
 //Перенести в слайс Юзера
-    [getProgress.pending]: (state)=>{
-      state.isLoading=true;
-      state.error=null;
-    },
-    [getProgress.fulfilled]: (state, {payload})=>{
-      state.training=[...state.training, payload];
-      state.isLoading=false;
-      state.error=null;
-    },
-    [getProgress.rejected]: (state, {payload})=>{
-      state.isLoading=false;
-      state.error=payload;
-      console.log("getProgress", payload);
-    }
-  }
-})
-
-
-// export default trainingSlice.reducer;
+    // [getProgress.pending]: (state)=>{
+    //   state.isLoading=true;
+    //   state.error=null;
+    // },
+    // [getProgress.fulfilled]: (state, {payload})=>{
+    //   state.training=[...state.training, payload];
+    //   state.isLoading=false;
+    //   state.error=null;
+    // },
+    // [getProgress.rejected]: (state, {payload})=>{
+    //   state.isLoading=false;
+    //   state.error=payload;
+    //   console.log("getProgress", payload);
+    // }
