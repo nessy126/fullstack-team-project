3eb030b130b53038a5ce5059e8586a76ea1137a6
Yevhen Bakhtizin <eugenbah@gmail.com>
2022-07-22 23:29:25 +0300

    bugFix/Chart

--- src/components/Chart/chartOptions.js
+++ src/components/Chart/chartOptions.js
@@ -10,60 +10,64 @@ const setTitle = (tooltipItems) => {
   return `Day ${title.label}`;
 };
 
-export const options = {
-  responsive: true,
-  maintainAspectRatio: false,
-  scales: {
-    y: {
-      ticks: {
-        display: true,
-        maxTicksLimit: 0,
-        beginAtZero: true,
+export const setOptions = (pagesToCheck) => {
+  const setTrueOrFalse = pagesToCheck > 0 ? true : false;
+  return {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        min: 0,
+        ticks: {
+          display: setTrueOrFalse,
+          maxTicksLimit: 0,
+          beginAtZero: true,
+        },
       },
-    },
-    x: {
-      grid: {
-        drawBorder: true,
-        color: COLORS.GRID,
-      },
-      ticks: {
-        beginAtZero: true,
-        display: true,
-        maxTicksLimit: 6,
-      },
-      title: {
-        display: true,
-        text: "Days",
-        align: "end",
-        color: COLORS.TEXT,
-        font: {
-          family: "Montserrat",
-          size: 12,
-          lineHeight: 3.17,
+      x: {
+        grid: {
+          drawBorder: true,
+          color: COLORS.GRID,
+        },
+        ticks: {
+          beginAtZero: true,
+          display: true,
+          maxTicksLimit: 6,
+        },
+        title: {
+          display: true,
+          text: "Days",
+          align: "end",
+          color: COLORS.TEXT,
+          font: {
+            family: "Montserrat",
+            size: 12,
+            lineHeight: 3.17,
+          },
         },
       },
     },
-  },
-  elements: {
-    point: {
-      radius: 0,
-      hitRadius: 5,
-    },
-  },
-  interaction: {
-    mode: "index",
-    intersect: false,
-  },
-  plugins: {
-    legend: { display: false },
-    tooltip: {
-      enabled: true,
-      position: "nearest",
-      titleAlign: "center",
-      backgroundColor: COLORS.GRID,
-      callbacks: {
-        title: setTitle,
+    elements: {
+      point: {
+        radius: 0,
+        hitRadius: 5,
       },
     },
-  },
+    interaction: {
+      mode: "index",
+      intersect: false,
+    },
+    plugins: {
+      legend: { display: false },
+      tooltip: {
+        enabled: setTrueOrFalse,
+        position: "nearest",
+        titleAlign: "center",
+        backgroundColor: COLORS.GRID,
+        callbacks: {
+          title: setTitle,
+        },
+      },
+    },
+  };
 };
