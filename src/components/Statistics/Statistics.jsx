09117f177cf1cf20aecc5ae79ea2a4a0d619a595
yurirusyn <iirusyn@gmail.com>
2022-07-24 16:26:31 +0200

    netlify

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,19 +1,13 @@
-// import StatisticsTabl from "components/StatisticsTabl";
 import CountdownTimer from "../CountdownTimer/CountdownTimer";
 import s from "./Statistics.module.scss";
-import { useSelector } from "react-redux";
-import trainingSelectors from "redux/training/trainingSelectors";
 import StatisticsResults from "components/StatisticsResults";
 const Statistics = () => {
-  // const dateTimeToEnd = useSelector(trainingSelectors.getAllTrainings);
   const now_in_ms = new Date().getTime();
   const getFullYear = new Date().getFullYear();
   const getFullYearMs = new Date(getFullYear + "-01-01").getTime();
   const newYearCountdown = Date.now() - getFullYearMs;
   const timeEndYear = ms_of_a_year(getFullYear) - newYearCountdown;
   const dateTimeToNewYear = now_in_ms + timeEndYear;
-  // const goalCountdown = dateTimeToEnd - now_in_ms;
-  // const dateTimeToGoal = now_in_ms + goalCountdown;
 
   function ms_of_a_year(year) {
     return isLeapYear(year)
@@ -37,7 +31,6 @@ const Statistics = () => {
           {/* <CountdownTimer targetDate={dateTimeToGoal} /> */}
         </div>
       </div>
-      {/* <StatisticsTabl /> */}
       <StatisticsResults></StatisticsResults>
     </>
   );
