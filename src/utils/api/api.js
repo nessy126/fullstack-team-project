5ba3b3dbc044f41e9d0726e6f6b35d060ad00cfe
nessy126 <nessy126@gmail.com>
2022-07-26 15:41:41 +0200

    +currentTraining

--- src/utils/api/api.js
+++ src/utils/api/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 //change
 function api() {
-axios.defaults.baseURL = "https://book-reader-team-project.herokuapp.com/";
-    // axios.defaults.baseURL = "http://localhost:8000/";
+// axios.defaults.baseURL = "https://book-reader-team-project.herokuapp.com/";
+    axios.defaults.baseURL = "http://localhost:8000/";
 }
 
 export default api;
