e5906b04381ffa57a638cec8ef31c849236956d5
nessy126 <nessy126@gmail.com>
2022-08-10 13:08:57 +0200

    added .env

--- src/utils/api/api.js
+++ src/utils/api/api.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 
+const { BASE_URL } = process.env;
+console.log(BASE_URL);
+
 //change
 function api() {
-  axios.defaults.baseURL = "https://book-reader-team-project.herokuapp.com/";
+  axios.defaults.baseURL = BASE_URL;
   //   axios.defaults.baseURL = "http://localhost:8000/";
+  //   axios.defaults.baseURL = "https://book-reader-team-project.herokuapp.com/";
 }
 
 export default api;
